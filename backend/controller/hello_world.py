686b725311570d56fe3350cbb98acef8422bf228
Cauê Melo <caue.melo@enext.com.br>
2021-07-29 18:46:12 -0300

    interface credentials

--- backend/controller/hello_world.py
+++ backend/controller/hello_world.py
@@ -1,8 +1,33 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+# import tweepy
 
+router = APIRouter()
 
-router = APIRouter(prefix="/hello")
+class Credentials(BaseModel):
+    accessToken: str
+    expiresIn: int
+    idToken: str
+    scope: str
+    tokenType: str
 
-@router.get("/hello", response_model=str)
-def hello() -> str:
+class Name(BaseModel):
+    name: str
+
+@router.get("/", response_model=str)
+def hello():
     return "Hello World!"
+
+
+@router.get("/teste")
+async def teste() -> dict:
+    item = {
+        "name": "caue",
+        "desc": "descricao"
+    }
+    return item
+
+@router.post("/login")
+async def login(name: Name):
+    print(name)
+
---
+++ nidus_app/android/app/src/main/kotlin/com/example/nidus_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nidus_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
