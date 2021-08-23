367f2b41b3f3185abd9c5e0db4f4dba79fa6e5f7
Cauê Melo <caue.melo@enext.com.br>
2021-08-23 20:13:16 -0300

    refactor(schemas): refactor database

--- .sql/nidus_database_core_v2.sql
+++ .sql/nidus_database_core_v2.sql
@@ -41,12 +41,43 @@ CREATE TABLE `session` (
 
 CREATE TABLE `notes` (
 	`id` varchar(255) NOT NULL,
+    `tweet_id` varchar(255),
     `user_id` varchar(255) NOT NULL,
-    `access_token` varchar(255) NOT NULL,
-    `active` boolean NOT NULL,
+    `content` varchar(300) NOT NULL,
     `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `end_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`),
-	KEY `session_user_fk` (`user_id`),
-	CONSTRAINT `session_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+	KEY `notes_user_fk` (`user_id`),
+	CONSTRAINT `notes_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `reminders` (
+	`id` varchar(255) NOT NULL,
+    `tweet_id` varchar(255),
+    `user_id` varchar(255) NOT NULL,
+    `content` varchar(300) NOT NULL,
+    `date` DATETIME NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `reminders_user_fk` (`user_id`),
+	CONSTRAINT `reminders_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tasklists` (
+	`id` varchar(255) NOT NULL,
+    `tweet_id` varchar(255),
+    `user_id` varchar(255) NOT NULL,
+    `content` varchar(300) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `tasklists_user_fk` (`user_id`),
+	CONSTRAINT `tasklists_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tasks` (
+	`id` varchar(255) NOT NULL,
+    `tasklist_id` varchar(255) NOT NULL,
+    `content` varchar(300) NOT NULL,
+	PRIMARY KEY (`id`),
+	KEY `tasks_tasklists_fk` (`tasklist_id`),
+	CONSTRAINT `tasks_tasklists_fk` FOREIGN KEY (`tasklist_id`) REFERENCES `tasklists` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- backend/controller/user.py
+++ backend/controller/user.py
@@ -13,15 +13,3 @@ session_repository: SessionRepository = Depends()
     if(session_repository.validate(authorization[7:], user_id)):
         return user_repository.find_by_id(user_id)
     return dict(error="Invalid token.")
-
-# @router.get("/")
-# async def get(
-# request: Request
-# ) -> None:
-#   # print(user_id)
-# #   print(getattr(request, 'headers'))
-#   print(request.__dict__)
-#     # if(session_repository.validate(access_token=access_token, 
-#     #   user_id=user_id) == False):
-#     #   return dict(error="Invalid token provided.")
-#     # return settings_repository.get(user_id)
