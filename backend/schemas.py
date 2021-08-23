254c18d8492a93606fb09c50df7609c916b2fdb2
Cauê Melo <cauesmelo@gmail.com>
2021-08-23 20:13:16 -0300

    refactor(schemas): refactor database

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import joinedload, lazyload, relationship
 from sqlalchemy.sql.functions import now, current_timestamp
-from sqlalchemy.sql.sqltypes import TIMESTAMP, Boolean
+from sqlalchemy.sql.sqltypes import DATETIME, TIMESTAMP, Boolean
 
 Base = declarative_base()
 
@@ -18,6 +18,9 @@ class User(Base):
     tw_email = Column(String)
     created_at = Column(TIMESTAMP, server_default=now())
     settings = relationship("Settings", backref="user", lazy='joined')
+    notes = relationship("Note", backref="user", lazy='joined')
+    reminders = relationship("Reminder", backref="user", lazy='joined')
+    tasklists = relationship("Tasklist", backref="user", lazy='joined')
 
 
 class Settings(Base):
@@ -39,39 +42,34 @@ class Session(Base):
     created_at = Column(TIMESTAMP, server_default=now())
     end_at = Column(TIMESTAMP)
 
-class RegisterType(Base):
-    __tablename__ = "tipo_registro"
+class Note(Base):
+    __tablename__ = "notes"
+    id = Column(String, primary_key=True)
+    tweet_id = Column(String)
+    user_id = Column(String, ForeignKey("user.id"),  unique=True, index=True)
+    content = Column(String)
+    created_at = Column(TIMESTAMP, server_default=now())
 
-    id = Column(Integer, primary_key=True)
-    nome = Column(String, nullable=False)
+class Reminder(Base):
+    __tablename__ = "reminders"
+    id = Column(String, primary_key=True)
+    tweet_id = Column(String)
+    user_id = Column(String, ForeignKey("user.id"),  unique=True, index=True)
+    content = Column(String)
+    date = Column(TIMESTAMP)
+    created_at = Column(TIMESTAMP, server_default=now())
 
-
-class Register(Base):
-    __tablename__ = "registro"
-
-    id = Column(Integer, primary_key=True)
-    tweet_content = Column(String)
-    data_hora_registro = Column(TIMESTAMP, nullable=True, default=current_timestamp)
-    tweet_id = Column(String, nullable=False, unique=True)
-    parent_tweet_id = Column(Integer, nullable=True)
-
-    usuario_id = Column(ForeignKey("usuario.id", name="registro_usuario_fk"), nullable=False, index=True)
-    tipo_registro = Column(ForeignKey("tipo_registro.id", name="registro_tipo_fk"), nullable=False)
-
-
-class TaskList(Base):
-    __tablename__ = "lista_tarefas"
-
-    id = Column(Integer, primary_key=True)
-    completo = Column(Integer, nullable=False, default=0)
-
-    registro_id = Column(ForeignKey("registro.id", name="lista_tarefas_registro_id_fk"), nullable=False, index=True)
-
+class Tasklist(Base):
+    __tablename__ = "tasklists"
+    id = Column(String, primary_key=True)
+    tweet_id = Column(String)
+    user_id = Column(String, ForeignKey("user.id"),  unique=True, index=True)
+    content = Column(String)
+    created_at = Column(TIMESTAMP, server_default=now())
+    tasks = relationship("Task", backref="tasklists", lazy='joined')
 
 class Task(Base):
-    __tablename__ = "tarefa"
-
-    id = Column(Integer, primary_key=True)
-    texto_tarefa = Column(String, nullable=True)
-    completo = Column(Boolean, nullable=False, default=False)
-    lista_id = Column(ForeignKey("lista_tarefas.id", name="tarefa_lista_id_fk"), nullable=False, index=True)
+    __tablename__ = "tasks"
+    id = Column(String, primary_key=True)
+    tasklist_id = Column(String, ForeignKey("tasklists.id"),  unique=True, index=True)
+    content = Column(String)
